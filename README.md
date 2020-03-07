96d13ee1be78479563e12989d2d3e45dd2c4b0ef
Nguyễn Bá Hưng <bahung1221@gmail.com>
2020-03-07 12:42:15 +0700

    init demo

---
+++ README.md
@@ -0,0 +1,7 @@
+- Open specific ios simulator:
+```
+xcrun simctl list
+# get udid of the device you want to launch
+# Paste this in the terminal
+open -a Simulator --args -CurrentDeviceUDID <DeviceUDID>
+```
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+    selector: "Login",
+    templateUrl: "./login.component.html",
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+    constructor(private router: Router) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    submit() {
+        this.router.navigate(['/home'])
+    }
+}
